50cb6cfc8650ab008fbac724cac99540a1fcf7bf
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-04-16 19:35:51 -0500

    Updates

---
+++ precache-manifest.78689195ef407c02abc8f020cef1d32c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efaa09c039fcd0f20b6775f21994d05",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "68463b76dd564fa294c2",
+    "url": "/react-redux-app/static/css/main.3f1cdfd3.chunk.css"
+  },
+  {
+    "revision": "a0411b820b6f575eb763",
+    "url": "/react-redux-app/static/js/2.ddf3d141.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-app/static/js/2.ddf3d141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68463b76dd564fa294c2",
+    "url": "/react-redux-app/static/js/main.ff621eda.chunk.js"
+  },
+  {
+    "revision": "3b1921a5e8d4f26d52bb",
+    "url": "/react-redux-app/static/js/runtime-main.d79e2e42.js"
+  },
+  {
+    "revision": "ffa437f03ee200afbb99a78dcd0868e7",
+    "url": "/react-redux-app/static/media/background.ffa437f0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Redux-App/precache-manifest.c7f59e472191fcc6bbd136fe2fa3e133.js"
+  "/react-redux-app/precache-manifest.78689195ef407c02abc8f020cef1d32c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Redux-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
