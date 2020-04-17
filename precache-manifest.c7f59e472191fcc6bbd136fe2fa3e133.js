4f616599eddd6624518910e711f13cd7bc35ecb8
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-04-16 19:31:49 -0500

    Updates

---
+++ precache-manifest.c7f59e472191fcc6bbd136fe2fa3e133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e856aebca09f65c0533772c8241d1ef2",
+    "url": "/React-Redux-App/index.html"
+  },
+  {
+    "revision": "080a8ed27639d70d87d5",
+    "url": "/React-Redux-App/static/css/main.1e09b456.chunk.css"
+  },
+  {
+    "revision": "a0411b820b6f575eb763",
+    "url": "/React-Redux-App/static/js/2.ddf3d141.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-App/static/js/2.ddf3d141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080a8ed27639d70d87d5",
+    "url": "/React-Redux-App/static/js/main.cc74781b.chunk.js"
+  },
+  {
+    "revision": "e08bf8f1beb254ee4fc6",
+    "url": "/React-Redux-App/static/js/runtime-main.57a280a4.js"
+  },
+  {
+    "revision": "ffa437f03ee200afbb99a78dcd0868e7",
+    "url": "/React-Redux-App/static/media/background.ffa437f0.jpg"
+  }
+]);
